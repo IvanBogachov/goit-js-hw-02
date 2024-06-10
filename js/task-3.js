4d5ef84b00351d719978189bac46c525c4462b6b
IvanBogachov <106016809+IvanBogachov@users.noreply.github.com>
2024-06-10 22:34:54 +0300

    commit message

--- js/task-3.js
+++ js/task-3.js
@@ -1,17 +1,12 @@
-function getElementWidth(content, padding, border) {
-  let contentWidth = Number.parseFloat(content);
-  let paddingWidth = Number.parseFloat(padding);
-  let bordertWidth = Number.parseFloat(border);
-
-  // console.log(contentWidth);
-  // console.log(paddingWidth);
-  // console.log(bordertWidth);
-
-  let totalWidth = contentWidth + paddingWidth * 2 + bordertWidth * 2;
-
-  return totalWidth;
+function checkForSpam(message) { 
+  const toLowerMass = message.toLowerCase();
+  return toLowerMass.includes("spam") || toLowerMass.includes("sale");
 }
+console.log(checkForSpam("Latest technology news")); // false
+console.log(checkForSpam("JavaScript weekly newsletter")); // false
+console.log(checkForSpam("Get best sale offers now!")); // true
+console.log(checkForSpam("Amazing SalE, only tonight!")); // true
+console.log(checkForSpam("Trust me, this is not a spam message")); // true
+console.log(checkForSpam("Get rid of sPaM emails. Our book in on sale!")); // true
+console.log(checkForSpam("[SPAM] How to earn fast money?")); // true
 
-console.log(getElementWidth("50px", "8px", "4px")); // 74
-console.log(getElementWidth("60px", "12px", "8.5px")); // 101
-console.log(getElementWidth("200px", "0px", "0px")); // 200
